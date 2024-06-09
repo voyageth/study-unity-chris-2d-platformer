6d6d6388b56822246fc5c61268ca703707a373ac
voyageth <voyageth@gmail.com>
2024-06-09 21:15:38 +0900

    Export Game Build and Make an Exit Game Action - 2D Platformer Crash Course in Unity 2022 (Part 24)

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -39,5 +41,23 @@ public class UIManager : MonoBehaviour
         Vector3 spawnPosition = Camera.main.WorldToScreenPoint(character.transform.position);
         TMP_Text tmpText = Instantiate(healthTextPrefab, spawnPosition, Quaternion.identity, gameCanvas.transform).GetComponent<TMP_Text>();
         tmpText.text = healthRestored.ToString();
+    }
+
+    public void OnExitGame(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            #if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+            Debug.Log(this.name + ":" + this.GetType() + ":" + System.Reflection.MethodBase.GetCurrentMethod().Name);
+            #endif
+
+            #if (UNITY_EDITOR)
+                UnityEditor.EditorApplication.isPlaying = false;
+            #elif (UNITY_STANDALONE)
+                Application.Quit();
+            #elif (UNITY_WEBGL)
+                SceneManager.LoadScene("QuitScene");
+            #endif
+        }
     }
 }
